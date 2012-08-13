95d5d9fe8937c068ffaae0a145c9c350cfaba3f7
gmarik <gmarik@gmail.com>
2012-08-12 20:05:51 -0500

    comments

--- process_daemonize.rb
+++ process_daemonize.rb
@@ -10,22 +10,31 @@ def daemonize!
   # the child process becomes session/process group leader
   Process.setsid
 
-   # the child process exists, spawning
-   # child2 process which goes on
-   exit if fork
+  # The forked process that had just become a process group and session group leader forks again and then exits.
+  exit if fork
+  # This newly forked process is no longer a process group leader nor a session leader. 
+  # Since the previous session leader had no controlling terminal, and this process is not a
+  # ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼session leader, it's guaranteed that this process can never have a controlling terminal. 
+  # Terminals can only be assigned to session leaders.
+  # ￼￼￼￼This dance ensures that our process is now fully detached from a controlling terminal and will run to its completion.
 
-   # set CWD(to avoid situation where current CWD gets deleted/unmounted/disappears)
-   Dir.chdir '/'
+  # This changes the current working directory to the root directory for the system. 
+  # This isn't strictly necessary but it's an extra step to ensure that 
+  # current working directory of the daemon doesn't disappear during its execution
+  Dir.chdir '/'
 
-   # reset any terminal input/output
-   STDIN.reopen '/dev/null'
-   STDOUT.reopen '/dev/null', 'a'
-   STDERR.reopen '/dev/null', 'a'
+  # This sets all of the standard streams to go to /dev/null , a.k.a. to be ignored. 
+  # Since the daemon is no longer attached to a terminal session these are of no use anyway. 
+  # They can't simply be closed because some programs expect them to always be available. 
+  # Redirecting them to /dev/null ensures that they're still available to the program but have no effect.
+  STDIN.reopen '/dev/null'
+  STDOUT.reopen '/dev/null', 'a'
+  STDERR.reopen '/dev/null', 'a'
 end
 
 daemonize!
 
-#store PID to make sure process starts
+#store PID to make sure process starts without error
 fname = File.basename(File.expand_path(__FILE__))
 File.open("/tmp/#{fname}.pid", 'w') do |f|
   f.write Process.pid
