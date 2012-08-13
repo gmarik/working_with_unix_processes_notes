ba6d0175cb8b45a8dfde84f03da0010a068a2958
gmarik <gmarik@gmail.com>
2012-08-13 16:36:06 -0500

    comments

--- ipc_pipe_parent_child.rb
+++ ipc_pipe_parent_child.rb
@@ -1,14 +1,15 @@
 reader, writer = IO.pipe
 
 fork do
-  reader.close # child process doesn't need this
+  # fork copies reader, writer to child process
+  reader.close # close child's copy
 
   10.times do
     writer.puts "Another one Bites the dust"
   end
 end
 
-writer.close # parent doesn't need this
+writer.close # close parent's copy
 
 while msg = reader.gets
   puts msg
