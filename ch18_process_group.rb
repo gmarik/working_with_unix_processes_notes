31e93fdfebdc2c122dbfe0f1d4a470619096f8c6
gmarik <gmarik@gmail.com>
2012-08-15 01:43:18 -0500

    commenting

--- ch18_process_group.rb
+++ ch18_process_group.rb
@@ -1,10 +1,19 @@
-# This example show how exiting process group by a process affects signal handling
 #
+# Process belong to process groups
+#
+
+#
+# This example show how interrupting parent process doesn't affect child
+# after child changes process group
+#
+
 trap(:INT) do
   puts "PID:#{Process.pid} received :INT signal, exiting..."
-  puts "Watch for zombie child"
+  puts "Watch for orphan child"
   exit(0)
 end
+
+puts "Press ^C to turn Child into an orphan"
 
 # every process is part of a Process Group
 puts "Parent PID:#{Process.pid} PGRP:#{Process.getpgrp}"
@@ -13,19 +22,17 @@ fork do
   puts "Child PID:#{Process.pid} PGRP:#{Process.getpgrp}"
 
   puts "Child quits parent's process group"
-  # this will cause Child to become part of new process group
+  # this will cause Child to become part of a new process group
   Process.setpgid(Process.pid, Process.pid)
 
   puts "Child PID:#{Process.pid} PGRP:#{Process.getpgrp} PPID #{Process.ppid}"
-
-  puts "Press ^C to turn Child into a zombie"
 
   5.times do
     puts "Child PID:#{Process.pid}, PPID: #{Process.ppid}"
     sleep 5
   end
 
-  puts "Zombie is Dead"
+  puts "Orphan child is now done"
 
   exit 0
 end
